5c75cf757574905365462d1ff8185c99c204a112
Om Sharma <omsharma.theplayer11@gmail.com>
2023-10-03 16:56:51 +0530

    car are not filtered on search

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,12 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+
+export interface FilterProps{
+  manufacturer : string,
+  year: number,
+  fuel: string,
+  limit: number,
+  model: string,
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
+import { link } from "fs";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "911855d3c7mshad0b2bd87b40fbcp17982ajsn2d5548fd177e",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
@@ -44,5 +47,5 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("modelYear", `${year}`);
   url.searchParams.append("angle", `${angle}`);
 
-  return `${url}`
+  return `${url}`;
 };
